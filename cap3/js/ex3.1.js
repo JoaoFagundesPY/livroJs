10bc897033b50771691572c3cd33b230c80bcb06
João Fagundes <joao.victor.fagundes1993@gmail.com>
2026-01-10 21:35:08 -0300

    Exemplo 3.1

---
+++ cap3/js/ex3.1.js
@@ -0,0 +1,13 @@
+function calcularMedia() {
+  // Cria referência aos elementos da Página
+  var inNome = document.getElementById("inNome");
+  var inNota1 = document.getElementById("inNota1");
+  var inNota2 = document.getElementById("inNota2");
+  var outSituacao = document.getElementById("outSituacao");
+  var outMedia = document.getElementById("outMedia");
+
+  // Obtém conteúdo dos campos de edição da página
+  var nome = inNome.value;
+  var nota1 = Number(inNota1.value);
+  var nota2 = Number(inNota2.value);
+}
