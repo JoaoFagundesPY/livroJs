881a064fa01f0540ae70f5e02d6d085fa63238c6
João Fagundes <joao.victor.fagundes1993@gmail.com>
2026-02-01 22:05:12 -0300

    Exemplo 3.1
    .1 – Programa calcula a média e, a partir de uma condição, exibe a situação do
    aluno.

--- cap3/js/ex3.1.js
+++ cap3/js/ex3.1.js
@@ -10,4 +10,24 @@ function calcularMedia() {
   var nome = inNome.value;
   var nota1 = Number(inNota1.value);
   var nota2 = Number(inNota2.value);
+
+  // Calcula a média das notas
+  let media = (nota1 + nota2) / 2;
+
+  // apresenta a média (Altera o conteúdo do elemento outMedia)
+  outMedia.textContent = "Média das Notas: " + media.toFixed(1);
+
+  // cria a condição
+  if (media >= 7) {
+    // altera o texto e estilo da cor do elemento outSituacao
+    outSituacao.textContent = "Parabéns " + nome + "! Você foi aprovado(a)";
+    outSituacao.style.color = "blue";
+  } else {
+    outSituacao.textContent = "Ops " + nome + "... Você foi reprovado(a)";
+    outSituacao.style.color = "red";
+  }
 }
+// cria uma referência ao elemento btResultado (botão)
+let btResultado = document.getElementById("btResultado");
+// Registra um evento associado ao botão, para carregar uma função
+btResultado.addEventListener("click", calcularMedia);
